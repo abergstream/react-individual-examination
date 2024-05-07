38f7bf511b8182ffde6a00ddab9ebc658e6ea5c3
Andreas Bergström <andreas@textalkgroup.com>
2024-05-07 11:16:29 +0200

    moved api-call and made contact-form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,36 @@
 import Icon from "@mdi/react";
 import { mdiAccountMultiple, mdiBriefcase, mdiChat } from "@mdi/js";
 import "./Contact.css";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 const Contact = () => {
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = () => {
+    let error = [];
+    !subject ? error.push("subject") : "";
+    !email ? error.push("e-mail") : "";
+    !message ? error.push("message") : "";
+    if (error.length > 0) {
+      toast.error(`Error! Missing ${error.join(", ")}`, {
+        duration: 8000,
+      });
+    } else {
+      setEmail("");
+      setMessage("");
+      setSubject("");
+      toast.success("Message sent.", { icon: "📨", duration: 5000 });
+    }
+  };
   return (
     <div className="contact-container">
       <section className="contact__form">
-        <h2>What do you want to say?</h2>
+        <h2 className="contact__heading">
+          How would you summarize the content of your email?
+        </h2>
         <article className="form__radio">
           <input
             className="form__radio-button"
@@ -13,6 +38,8 @@ const Contact = () => {
             type="radio"
             name="subject"
             value="Hire"
+            checked={subject === "Hire"}
+            onChange={(e) => setSubject(e.target.value)}
           />
           <label htmlFor="input_hire" className="form__radio-label">
             <Icon path={mdiBriefcase} size={4} />
@@ -24,6 +51,8 @@ const Contact = () => {
             type="radio"
             name="subject"
             value="Collaboration"
+            checked={subject === "Collaboration"}
+            onChange={(e) => setSubject(e.target.value)}
           />
 
           <label htmlFor="input_collab" className="form__radio-label">
@@ -36,6 +65,8 @@ const Contact = () => {
             type="radio"
             name="subject"
             value="Chat"
+            checked={subject === "Chat"}
+            onChange={(e) => setSubject(e.target.value)}
           />
 
           <label htmlFor="input_chat" className="form__radio-label">
@@ -43,6 +74,21 @@ const Contact = () => {
             Chat
           </label>
         </article>
+        <input
+          className="form__input"
+          placeholder="Your e-mail address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <textarea
+          className="form__input form__input--textarea"
+          placeholder="Content"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <button className="form__button" onClick={handleSubmit}>
+          Send
+        </button>
       </section>
     </div>
   );
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,14 +1,40 @@
 import "./Projects.css";
-import "../../components/FetchRepos/FetchRepos.css";
-import FetchRepos from "../../components/FetchRepos/FetchRepos";
 import RotatingCircle from "../../components/RotatingCircle/RotatingCircle";
 import RepoCard from "../../components/RepoCard/RepoCard";
-
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 import Button from "../../components/Button/Button";
 const Projects = ({ repos, setRepos }) => {
+  const API_USER = "abergstream";
+  const API_URL = `https://api.github.com/users/${API_USER}/repos?page=1&per_page=300`;
+  useEffect(() => {
+    const getRepos = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        data.message === "Not Found"
+          ? toast.error(`Could not find user: ${API_USER}`, {
+              duration: 8000,
+            })
+          : toast.success("API loaded.", { icon: "👌🏻", duration: 3000 });
+        const sortedRepos = sortByDate([...data]).slice(0, 3);
+        setRepos(sortedRepos);
+        console.log(sortedRepos);
+      } catch (error) {
+        toast.error(`Could not connect to ${API_URL}`, {
+          duration: 8000,
+          style: {
+            minWidth: "520px",
+          },
+        });
+      }
+    };
+    if (!repos) {
+      getRepos();
+    }
+  }, [API_URL, repos]);
   return (
     <>
-      {/* <FetchRepos setRepos={setRepos} repos={repos} /> */}
       <section>
         <h2 className="page__header">My Projects!</h2>
         <div>
@@ -51,5 +77,10 @@ const Projects = ({ repos, setRepos }) => {
     </>
   );
 };
-
+const sortByDate = (sortedData) => {
+  sortedData.sort((a, b) => {
+    return new Date(b.updated_at) - new Date(a.updated_at);
+  });
+  return sortedData;
+};
 export default Projects;
